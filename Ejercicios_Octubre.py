4358685a1f639a96112d92ee4d6f0c2e51956d06
Dario Lucas Tamini <111819468+Daro2080@users.noreply.github.com>
2022-10-14 21:18:51 -0300

    Update Ejercicios_Octubre.py

--- Ejercicios_Octubre.py
+++ Ejercicios_Octubre.py
@@ -1,1 +1,23 @@
+# CLASE PRESENTADA POR DARIO TAMINI
 
+class Rectangulo:
+    '''
+    crear una clase llamada rectangulo, debe tener 2 atributos: altura y base
+    el nombre del metodo sera calcular area utilizando la formula:
+    area = base * altura
+    pero la base y la altura deben ser ingresadas por
+    el usuario y los objetos deben ser 3
+    '''
+
+    def __init__(self, altura, base):
+        self.altura = altura
+        self.base = base
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+
+base = int(input("Digite el numero para la base del rectangulo: "))
+altura = int(input("Digite el numero para la altura del rectangulo: "))
+rectangulo1 = Rectangulo(base, altura)
+print(f"El area del rectangulo es: {rectangulo1.calcular_area()}")
