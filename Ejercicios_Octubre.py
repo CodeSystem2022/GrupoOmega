ed3e3cb32a758d9eccd4f92fbee168cea5829993
Gustavo DeLlac <103227982+gusdellac@users.noreply.github.com>
2022-10-15 19:36:43 -0300

    commit ejercicio python Octubre Gustavo De Llac

--- Ejercicios_Octubre.py
+++ Ejercicios_Octubre.py
@@ -21,3 +21,28 @@ base = int(input("Digite el numero para la base del rectangulo: "))
 altura = int(input("Digite el numero para la altura del rectangulo: "))
 rectangulo1 = Rectangulo(base, altura)
 print(f"El area del rectangulo es: {rectangulo1.calcular_area()}")
+
+
+# EJERCICIO PRESENTADO POR GUSTAVO DE LLAC
+
+"""
+Crear clase Cubo con los atributos: ancho, alto y profundidad.
+Con un método calcular_volumen que tendrá la formula:
+volumen = ancho * altura * profundidad.
+El usuario debe ingresar los valores
+"""
+class Cubo:
+    def __init__(self, ancho, altura, profundidad):
+        self.ancho = ancho
+        self.altura = altura
+        self.profundidad = profundidad
+
+    def calcular_volumen(self):
+        return self.ancho * self.altura * self.profundidad
+
+print(">>>>Cálculo volumen de un cubo<<<<")
+ancho = float(input("Digite ancho del cubo: "))
+altura = float(input("Digite altura del cubo: "))
+profundidad = float(input("Digite profundidad del cubo: "))
+cubo1 = Cubo(ancho, altura, profundidad)
+print(f"El volumen del cubo es: {cubo1.calcular_volumen()}")
