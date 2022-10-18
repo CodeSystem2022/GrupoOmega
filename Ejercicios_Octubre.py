6aa30419014585e454a83cf831e7a86906ca232a
Leonel Real <112029499+Leonel1109@users.noreply.github.com>
2022-10-17 21:19:02 -0300

    Update Ejercicios_Octubre.py

--- Ejercicios_Octubre.py
+++ Ejercicios_Octubre.py
@@ -46,3 +46,35 @@ altura = float(input("Digite altura del cubo: "))
 profundidad = float(input("Digite profundidad del cubo: "))
 cubo1 = Cubo(ancho, altura, profundidad)
 print(f"El volumen del cubo es: {cubo1.calcular_volumen()}")
+
+# EJERCICIO PRESENTADO POR LEONEL REAL
+
+class Aritmetica:
+    """
+    El nombre de este tipo de comentario es: DocString
+    esto es documentación de la clase en Python
+    Vamos a hacer en esta clase algunas operaciones de: suma, resta, multipliciación y más
+    """
+
+    def __init__(self, operandoA, operandoB):
+        self.operandoA = operandoA
+        self.operandoB = operandoB
+
+    # Método para sumar
+    def sumar(self):
+        return self.operandoA + self.operandoB
+
+    def resta(self):
+        return self.operandoA - self.operandoB
+
+    def multiplicar(self):
+        return self.operandoA * self.operandoB
+
+    def dividir(self):
+        return self.operandoA / self.operandoB
+
+aritmetica1 = Aritmetica(7, 9)  # Le pasamos los argumentos para los operadores
+print(f'La suma de los números es: {aritmetica1.sumar()}')
+print(f'La resta de los números es: {aritmetica1.resta()}')
+print(f'La multiplicación de los números es: {aritmetica1.multiplicar()}')
+print(f'La division de los números es: {aritmetica1.dividir():.2f}')
