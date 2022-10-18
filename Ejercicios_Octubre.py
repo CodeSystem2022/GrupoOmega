d0f1cc8e607533ddd6682d0c46a25564c05f1e79
JULIETA GUTIERREZ <112029156+julyisis@users.noreply.github.com>
2022-10-17 21:35:37 -0300

    Update Ejercicios_Octubre.py

--- Ejercicios_Octubre.py
+++ Ejercicios_Octubre.py
@@ -79,4 +79,28 @@ print(f'La resta de los números es: {aritmetica1.resta()}')
 print(f'La multiplicación de los números es: {aritmetica1.multiplicar()}')
 print(f'La division de los números es: {aritmetica1.dividir():.2f}')
 
+# Tarea realizada Julieta Gutierrez
+
+class Cubo:
+    """
+    Crear la clase Cubo con los atributos,ancho,alto,y profundidad ,con
+    un método calcular_volumen que tendra la formula:
+    volumen= ancho * altura * profundidad
+    que el usuario ingrese los valores
+    """
+    def __init__(self,ancho,alto,profundidad):
+        self.ancho = ancho
+        self.alto = alto
+        self.profundidad = profundidad
+    def calcular_volumen(self):
+        return self.ancho * self.alto * self.profundidad
+
+ancho = int(input('Digite el ancho del cubo: '))
+alto = int(input('Digite el alto para el cubo: '))
+profundidad = int(input('Digite a profundidad del cubo: '))
+
+cubo1 = Cubo(ancho,alto,profundidad)
+print(f'El volumen del cubo es: {cubo1.calcular_volumen()}') # Al llamar al objeto cubo1 al método calcular_volumen
+
+
 
