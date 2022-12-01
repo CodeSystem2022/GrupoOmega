5f6eacea11337c4171ffc2d7cfdbff1771e8eb77
Gaston091218 <112030267+Gaston091218@users.noreply.github.com>
2022-11-30 22:23:36 -0300

    Update Asistencia_Noviembre.py

--- Asistencia_Noviembre.py
+++ Asistencia_Noviembre.py
@@ -185,3 +185,41 @@ persona2= Persona('Betancud', 5)
 
 print (personal + persona2)
 print (personal - persona2)
+        
+        #Clase Presentada por Gaston Riveros
+        class Vehiculo:
+## Crear un objeto de cada Clase
+
+    def __init__(self, color, ruedas):
+        self.color =color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return 'Color :' + self.color+', Ruedas'+ str(self.ruedas)
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+
+    def __int__(self):
+        return super().__str__()+', Velocidad(km/hr): ' + str(self.velocidad)
+
+    class Bicicicleta(Vehiculo):
+        def __int__(self, color, ruedas, tipo):
+            super().__init__(color, ruedas)
+            self.tipo = tipo
+
+        def __str__(self):
+            return super().__str__()+' Tipo: '+ self.tipo
+
+    # Primer objeto de la clase padre Vehiculo
+    vehiculo = Vehiculo('Blanco', 4)
+    print(vehiculo)
+
+    # Segundo Objeto, pero ahora de la clase Auto
+    auto= Auto( 'Amarillo', 4, 120)
+    print(auto)
+
+    # Tercer objeto, el ultimo de la clase Bicicleta
+    bici = Bicicicleta('Azul', 2, 'Urbana')
+    print(bici)
